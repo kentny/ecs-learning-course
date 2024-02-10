1288ff865062e05908854ad506446491230807e4
kentny <kentaro.terasaki@gmail.com>
2024-02-10 09:24:45 +0900

    Modify the url used in FeignClient.

--- service-communication-section/order/src/main/java/org/example/order/client/InventoryClient.java
+++ service-communication-section/order/src/main/java/org/example/order/client/InventoryClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "inventory-client", url = "http://inventory:8080/api/inventories")
+@FeignClient(name = "inventory-client", url = "${inventory.client.url}")
 public interface InventoryClient {
     @PostMapping("/decrease")
     PostInventoryDecreaseResponse postDecrease(@RequestBody PostInventoryDecreaseRequest request);
--- service-communication-section/product/src/main/java/org/example/product/client/InventoryClient.java
+++ service-communication-section/product/src/main/java/org/example/product/client/InventoryClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "inventory-client", url = "http://inventory:8080/api/inventories")
+@FeignClient(name = "inventory-client", url = "${inventory.client.url}")
 public interface InventoryClient {
     @GetMapping("/{productId}")
     GetInventoryDecreaseResponse get(@PathVariable Long productId);
