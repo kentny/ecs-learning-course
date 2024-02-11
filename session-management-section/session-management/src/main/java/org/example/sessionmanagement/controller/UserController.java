f0286af58ebbf33648c59b1e7b26834a2667ab9c
kentny <kentaro.terasaki@gmail.com>
2024-02-11 17:58:16 +0900

    Add Session Management section.

---
+++ session-management-section/session-management/src/main/java/org/example/sessionmanagement/controller/UserController.java
@@ -0,0 +1,38 @@
+package org.example.sessionmanagement.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final HttpSession httpSession;
+
+    public UserController(HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
+
+    @GetMapping("/status")
+    ResponseEntity<String> status() {
+        if (this.httpSession.getAttribute("login-status") == null) {
+            return ResponseEntity.ok("You Are 'Logged Out'");
+        }
+        return ResponseEntity.ok("You Are 'Logged In'");
+    }
+
+    @PostMapping("/login")
+    ResponseEntity<String> login() {
+        this.httpSession.setAttribute("login-status", "Logged In");
+        return ResponseEntity.ok("Logged In");
+    }
+
+    @PostMapping("/logout")
+    ResponseEntity<String> logout() {
+        this.httpSession.invalidate();
+        return ResponseEntity.ok("Logged Out");
+    }
+}
